3ca0572aba6dce7d2aab4abc4abf758ded39b36b
luke-mayer <lemayer214@gmail.com>
2025-02-06 01:20:13 -0500

    Messing around with next in express

--- api-basic/src/index.js
+++ api-basic/src/index.js
@@ -15,6 +15,28 @@ app.get('/params/:p', (req, res) => {
   res.send(req.params);
 });
 
+// Trying out next()
+const nextA = function (req, res, next) {
+  console.log('A\n');
+  next();
+};
+
+const nextB = function (req, res, next) {
+  console.log('B\n');
+  next();
+};
+
+const nextC = function (req, res) {
+  res.send('C');
+};
+
+const nextArr = [];
+nextArr.push(nextA);
+nextArr.push(nextB);
+nextArr.push(nextC);
+
+app.get('/next', nextArr);
+
 app.listen(port, () => {
   console.log(`API app listening on port ${port}`);
 });

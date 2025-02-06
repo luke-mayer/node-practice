14ac8f66fc0d1dcc6ca6a73ea355c5569986855a
luke-mayer <lemayer214@gmail.com>
2025-02-06 00:54:06 -0500

    Started a basic node/express api

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# node-practice
+# Node.js Practice
+
+Just a repository where I mess around with Node.js to learn new things,
+practice fundamentals, build demos/toy projects, or experiment with new
+libraries/tools.
---
+++ api-basic/src/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`API app listening on port ${port}`);
+});
